690e52c1f81d9490befe4ac8df48798e7d32a0b1
solahkay <solahudinalmalibari@gmail.com>
2023-08-22 13:05:38 +0700

    add: entity Menu and Order

---
+++ src/main/java/solahkay/binar/challenge/entity/Menu.java
@@ -0,0 +1,52 @@
+package solahkay.binar.challenge.entity;
+
+import java.util.Objects;
+
+public class Menu {
+
+    private Long id;
+    private String itemName;
+    private Integer price;
+
+    public Menu() {}
+
+    public Menu(Long id, String itemName, Integer price) {
+        this.id = id;
+        this.itemName = itemName;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "MenuList{" +
+                "id=" + id +
+                ", menu='" + itemName + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+}
---
+++ src/main/java/solahkay/binar/challenge/entity/Order.java
@@ -0,0 +1,49 @@
+package solahkay.binar.challenge.entity;
+
+public class Order {
+
+    private Menu menu;
+
+    private Integer total;
+    private Long totalAmount;
+
+    public Order(Menu menu, Integer total) {
+        this.menu = menu;
+        this.total = total;
+        this.totalAmount = ((long) menu.getPrice() * total);
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public void setMenu(Menu menu) {
+        this.menu = menu;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public Long getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(Long totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "menu=" + menu +
+                ", total=" + total +
+                ", totalAmount=" + totalAmount +
+                '}';
+    }
+
+}
