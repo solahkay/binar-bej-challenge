638876ca51ef432bb7cfd809dde073b6e8925a4b
solahkay <solahudinalmalibari@gmail.com>
2023-08-23 19:14:26 +0700

    add: Receipt util class

---
+++ src/main/java/solahkay/binar/challenge/util/ReceiptUtil.java
@@ -0,0 +1,50 @@
+package solahkay.binar.challenge.util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ReceiptUtil {
+
+    private ReceiptUtil() {}
+
+    public static String createReceipt(String orderList) {
+        String wrapper = "==========================";
+        String delimiter = System.getProperty("line.separator");
+
+        return String.join(
+                delimiter,
+                wrapper,
+                "BinarFud",
+                wrapper,
+                "",
+                "Terima kasih sudah memesan",
+                "di BinarFud",
+                "",
+                "Di bawah ini adalah pesanan anda",
+                "",
+                orderList,
+                "",
+                "Pembayaran : BinarCash",
+                "",
+                wrapper,
+                "Simpan struk ini sebagai",
+                "bukti pembayaran",
+                wrapper
+        );
+    }
+
+    public static void writeReceiptToTxtFile(String receipt) {
+        Path path = Paths.get("receipt.txt");
+        byte[] receiptBytes = receipt.getBytes();
+
+        try {
+            Files.write(path, receiptBytes);
+            System.out.println("\nStruk berhasil dicetak di receipt.txt");
+        } catch (IOException exception) {
+            System.out.println("\nStruk gagal dicetak");
+        }
+    }
+
+}
