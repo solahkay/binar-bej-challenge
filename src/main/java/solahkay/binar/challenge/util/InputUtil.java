7eab68c2fa339d833f32bd9d8d1944798df8004d
solahkay <solahudinalmalibari@gmail.com>
2023-09-09 22:12:58 +0700

    refactoring: change name case of scanner variable

--- src/main/java/solahkay/binar/challenge/util/InputUtil.java
+++ src/main/java/solahkay/binar/challenge/util/InputUtil.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class InputUtil {
 
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
 
     private InputUtil() {
     }
 
     public static String input(String message) {
         System.out.print(message + " ");
-        return scanner.nextLine();
+        return SCANNER.nextLine();
     }
 
 }
